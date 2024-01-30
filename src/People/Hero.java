8f3ab86a7a46f6688638ba1b2e56e9f56e75d4c2
Meirzhan05 <saparovmeirzhan@gmail.com>
2024-01-29 20:47:42 -0500

    Added functions for adding items to Notebook

--- src/People/Hero.java
+++ src/People/Hero.java
@@ -1,6 +1,10 @@
 package People;
 import Weapons.*;
+import Inventory.*;
+
 public class Hero extends Person {
+    Weapon currWeapon;
+    Notebook notebook;
     public Hero(String name,
                 String role,
                 String description,
@@ -10,11 +14,29 @@ public class Hero extends Person {
                 String location){
         super(name, role,description,isSuspect,relationshipWithVictim,isUseful,location);
         Knife knife= new Knife();
-        currWeapon = knife;
+        this.currWeapon = knife;
+        notebook = new Notebook();
         //constructor would print the Bio/Background of our hero, we can un comment the below
         //- line later when we figure out the GUI stuff
-
-        //System.out.println(description);
     }
 
+    public void setCurrWeapon(Weapon currWeapon) {
+        this.currWeapon = currWeapon;
+    }
+
+    public Weapon getCurrWeapon() {
+        return this.currWeapon;
+    }
+
+    public void addClue(Clue clue) {
+        notebook.addClue(clue);
+    }
+
+    public void addAward(Award award) {
+        notebook.addAward(award);
+    }
+
+    public void addPerson(Person person) {
+        notebook.addPerson(person);
+    }
 }
