e5dc83633e362a179282a5c830fd10df7b950bef
Meirzhan "MJ" Saparov <101402535+Meirzhan05@users.noreply.github.com>
2024-02-09 18:37:21 -0500

    Deleted extra code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,4 @@
-import Inventory.Clue;
-import People.*;
-import Utility.JsonUtil;
-import com.fasterxml.jackson.core.type.TypeReference;
-import Location.*;
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        List<Location> locations = JsonUtil.parseJsonFile("src/Resources/Locations.json", new TypeReference<List<Location>>() {});
-        for (Location i : locations) {
-            System.out.println(i.getName());
-        }
+    public static void main(String[] args) {
     }
 }
